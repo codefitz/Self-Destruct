4e19c154eae061111263617d4c6cda0a3779c568
Wesley Moskal-Fitzpatrick <github@fitzmoskal.me>
2025-07-08 01:22:01 +0100

    Fix flake8 warnings

--- generate_key.py
+++ generate_key.py
@@ -8,8 +8,7 @@ import datetime
 import base64
 import sys
 import time
-  
- 
+
 now = datetime.date.today()
 days = 30
 future = now + datetime.timedelta(days=days)
@@ -17,13 +16,13 @@ expiry = future.timetuple()
 
 timestamp = time.mktime(expiry)
 
-print("Generating key, expiry date: ",future)
-secret = [ timestamp, "random_key" ]
+print("Generating key, expiry date: ", future)
+secret = [timestamp, "random_key"]
 
-b64key= base64.b64encode(str(secret).encode('utf-8'))
+b64key = base64.b64encode(str(secret).encode('utf-8'))
 key = bytes.decode(b64key)
 
-print("Generated Key: %s\n"%key)
-print("Key will expire in %i days."%days)
+print("Generated Key: %s\n" % key)
+print("Key will expire in %i days." % days)
 
 sys.exit(0)
